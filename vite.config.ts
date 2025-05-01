b56613bc488333ba9726d4ea1b71deb0ca9ffa0e
Carlos Eduardo <127767516+CarlosEData@users.noreply.github.com>
2025-05-01 13:29:59 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ESSENCIAL para GoDaddy funcionar corretamente
+  assetsInclude: ['**/*.PNG'],
   plugins: [react()],
-  server: {
+  server:{
     open: true,
   },
   optimizeDeps: {
-    exclude: ['Lucide-react'],
+    exclude: ['lucide-react'],
   },
 });
